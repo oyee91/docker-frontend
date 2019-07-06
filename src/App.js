8ec316a9c71138dbb86ab953984f9e63357b5a7f
Josef <josef.oygun@scania.com>
2019-07-06 13:28:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          3e3 <code>src/App.js</code> andsa da1d dto 1we212.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reeact
         </a>
       </header>
     </div>
