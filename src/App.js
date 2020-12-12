1a8f77d39387985694688b8d21b128d46dcaa343
oyee91 <josefoygun@gmail.com>
2020-12-12 18:50:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reeact
+          Learn Reeact in feature branch
         </a>
       </header>
     </div>
